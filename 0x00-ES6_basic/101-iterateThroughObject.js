46e22644af7b2a336d81ffa0b3b6b2b9e73239a8
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-02 19:20:17 +0100

    Using an Iterator

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let names = '';
+  let first = true;
+  for (const rep of reportWithIterator) {
+    if (!first) {
+      names += ' | ';
+    }
+    if (rep !== undefined) {
+      names += rep;
+    }
+    first = false;
+  }
+  return names;
+}
