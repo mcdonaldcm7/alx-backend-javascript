94de6121cf508b80fd0e74021964691764d9ac85
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-03 09:27:34 +0100

    Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const ret = {};
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises).then((results) => results);
+
+  return Promise.allSettled(promises).then((results) => {
+    for (const resp of results) {
+      console.log(resp);
+      ret.status = resp.status;
+      ret.value = resp.value;
+    }
+  });
 }
