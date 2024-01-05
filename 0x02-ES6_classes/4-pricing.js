ff9646c22c625eadf5c37e3a0e243bf5e7dd9cef
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-05 02:52:58 +0100

    Simple implementation of a static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be of type Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion Rate must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}
