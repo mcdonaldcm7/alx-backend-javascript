4613c044cde7624100f6b2e1922f37089b0ccdea
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-05 02:15:28 +0100

    Implementing getters and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students) || !students.every((item) => typeof item === 'string')) {
+      throw TypeError('Students must an array contain only strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
+}
