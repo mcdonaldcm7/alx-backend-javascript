eff742d7d3eec3212e999345aa31d5542747a9d3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-05 03:11:52 +0100

    Implementing an Abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,27 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
