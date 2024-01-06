93b6fb4e3bdfee9b1e4185f4b1dd8b988bdfb00f
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-06 05:22:48 +0100

    Returning a Car clone

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,27 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    if (typeof brand !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    if (typeof motor !== 'string') {
+      throw new TypeError('Motor must be a string');
+    }
+    if (typeof color !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
+    if (typeof range !== 'string') {
+      throw new TypeError('Range must be a string');
+    }
+    super(brand, motor, color);
+    // this._brand = brand;
+    // this._motor = motor;
+    // this._color = color;
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car();
+  }
+}
