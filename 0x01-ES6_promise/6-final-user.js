0fbfdd760e4a98721673d4cf9fef7ba3a517a216
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-06 05:41:59 +0100

    Formatted return object

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => response);
+    .then((results) => {
+      const responses = [];
+      results.forEach((result) => {
+        responses.push({
+          status: result.status,
+          value: result.value,
+        });
+      });
+      return responses;
+    });
 }
