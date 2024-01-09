f6525b4fa500088d735da2b8da6e7735658b3c1b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-01-09 05:20:32 +0100

    A simple function to return a string of all the set of values that start with a specific string

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  let all = '';
+  let sep = false;
+  for (const x of set) {
+    if (x.startsWith(startString) && startString.length > 1) {
+      if (sep) {
+        all += '-';
+      }
+      all += x.substr(startString.length);
+      if (!sep) {
+        sep = true;
+      }
+    }
+  }
+  return all;
+}
