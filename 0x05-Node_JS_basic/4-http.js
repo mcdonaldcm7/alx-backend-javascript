5a6fb11c6665c192d028469834654674278503b4
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-27 06:22:33 +0100

    A small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+const http = require('node:http');
+
+const hostname = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
