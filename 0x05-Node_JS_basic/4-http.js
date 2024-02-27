938720239047fc32aa7186d073d0251ef1f99efc
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-27 14:13:29 +0100

    Formatted code to use less lines

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,11 @@
 const http = require('node:http');
 
-const hostname = 'localhost';
-const port = 1245;
-
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.end('Hello Holberton School!\n');
 });
 
-app.listen(port, hostname, () => {
+app.listen(1245, 'localhost', () => {
   console.log('...');
 });
 
