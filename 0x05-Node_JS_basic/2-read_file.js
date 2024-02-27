52c040252062eafaf375392383ae173de9f1737d
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-27 13:32:35 +0100

    Fixed ESLint errors

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -16,15 +16,15 @@
 
 const fs = require('node:fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n');
     lines.pop();
-    console.log('Number of students: ' + (lines.length - 1));
+    console.log(`Number of students: ${lines.length - 1}`);
     const studentsFields = {};
     // Fetches all the fields and adds them to a set
-    for (let i = 1; i < lines.length; i++) {
+    for (let i = 1; i < lines.length; i += 1) {
       const line = lines[i].split(',');
       const name = line[0];
       const field = line[(line.length - 1)];
@@ -36,10 +36,11 @@ function countStudents (path) {
       }
     }
     for (const k in studentsFields) {
-      console.log(`Number of students in ${k}: ${studentsFields[k].length}. List: ${studentsFields[k]}`);
+      if (k !== undefined) {
+        console.log(`Number of students in ${k}: ${studentsFields[k].length}. List: ${studentsFields[k].join(', ')}`);
+      }
     }
   } catch (error) {
-    console.log(error);
     throw new Error('Cannot load the database');
   }
 }
