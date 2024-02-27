c366f37758e2b2d78f9778f71d0ebb94c50cd305
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-27 14:20:26 +0100

    Set Content-Type to text.plain

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,7 @@
 const http = require('node:http');
 
 const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
 
