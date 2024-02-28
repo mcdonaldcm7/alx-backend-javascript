8594fe762532d66f6ffa15e80126bd9844d21253
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-02-28 17:57:19 +0100

    Handled case: Non-number arguments passed

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,4 +1,8 @@
 function calculateNumber(a, b) {
+  if (a === undefined || b === undefined || typeof(a) !== 'number' || typeof(b)
+      !== 'number') {
+    throw TypeError("arguments must be of type number");
+  }
   return (Math.round(a) + Math.round(b));
 }
 
