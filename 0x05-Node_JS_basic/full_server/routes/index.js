fa422f11874610c8b8b617cf0f3c2eb09d78ca8c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-04 06:09:31 +0100

    Router to link paths

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,23 @@
+// const express = require('express');
+// const AppController = require('../controllers/AppController');
+// const StudentsController = require('../controllers/StudentsController');
+
+import express from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  AppController.getHomepage(req, res);
+});
+
+router.get('/students', (req, res) => {
+  StudentsController.getAllStudents(req, res)
+});
+router.get('/students/:major', (req, res) => {
+  StudentsController.getAllStudentsByMajor(req, res)
+});
+
+// module.exports = router;
+export default router;
