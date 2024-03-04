a561bb402e66abd7f506e9a53aed507e0663ee3c
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-04 06:18:30 +0100

    Fixed lint errors

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -8,23 +8,23 @@ class StudentsController {
     readDatabase(path)
       .then((result) => {
         let responseMessage = 'This is the first list of our students\n';
-        // Sorts the key alphabetically (Case insensitive)	      
+        // Sorts the key alphabetically (Case insensitive) 
         const sortedKeys = Object.keys(result).sort((a, b) => a.localeCompare(b));
 
         for (const key of sortedKeys) {
-          const students = result[key]
+          const students = result[key];
           const numStudents = students.length;
           responseMessage += `Number of students in ${key}: ${numStudents}. List: ${students.join(', ')}\n`;
         }
         response.status(200).send(responseMessage);
       })
-      .catch((error) => {
-	response.status(500).send('Cannot load the database');
+      .catch(() => {
+        response.status(500).send('Cannot load the database');
       });
   }
 
   static getAllStudentsByMajor(request, response) {
-    const param = request.params['major']
+    const param = request.params.major;
     if (param !== 'CS' && param !== 'SWE') {
       response.status(500).send('Major parameter must be CS or SWE');
       return;
@@ -33,7 +33,7 @@ class StudentsController {
       .then((result) => {
         response.status(200).send(`List: ${result[param].join(', ')}`);
       })
-      .catch((error) => {
+      .catch(() => {
         response.status(500).send('Cannot load the database');
       });
   }
