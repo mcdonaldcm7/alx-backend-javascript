a423875b7848949010c67848d11d5dee67693fa6
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-04 06:08:10 +0100

    Defines the homepage route

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,6 +1,8 @@
 class AppController {
   static getHomepage(request, response) {
-   // Returns 200 status and the message 'Hello Holberton School!'
-   
+    // Returns 200 status and the message 'Hello Holberton School!'
+    response.status(200).send('Hello Holberton School!');
   }
 }
+
+export default AppController;
