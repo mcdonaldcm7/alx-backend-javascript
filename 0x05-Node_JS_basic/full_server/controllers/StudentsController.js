c475e5db4160d36b41d0aa8d988ecf754cce3705
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2024-03-04 06:08:53 +0100

    Defines the handler for the path /students and /students/:major

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,17 +1,42 @@
-import express;
+// const readDatabase = require('../utils');
+import readDatabase from '../utils';
 
-const readDatabase = require('../utils').readDatabase;
+const path = process.argv[2];
 
 class StudentsController {
   static getAllStudents(request, response) {
-    // Return a status 200
-    let resp = 'This is the list of our students';
-    readDatabase(response.url.replace('/', ''))
-      .then(() => {
+    readDatabase(path)
+      .then((result) => {
+        let responseMessage = 'This is the first list of our students\n';
+        // Sorts the key alphabetically (Case insensitive)	      
+        const sortedKeys = Object.keys(result).sort((a, b) => a.localeCompare(b));
+
+        for (const key of sortedKeys) {
+          const students = result[key]
+          const numStudents = students.length;
+          responseMessage += `Number of students in ${key}: ${numStudents}. List: ${students.join(', ')}\n`;
+        }
+        response.status(200).send(responseMessage);
       })
       .catch((error) => {
-        response.status = 500;
-        response.render('500', { error: 'Cannot load the database'  });
+	response.status(500).send('Cannot load the database');
+      });
+  }
+
+  static getAllStudentsByMajor(request, response) {
+    const param = request.params['major']
+    if (param !== 'CS' && param !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+    readDatabase(path)
+      .then((result) => {
+        response.status(200).send(`List: ${result[param].join(', ')}`);
+      })
+      .catch((error) => {
+        response.status(500).send('Cannot load the database');
       });
   }
 }
+
+export default StudentsController;
